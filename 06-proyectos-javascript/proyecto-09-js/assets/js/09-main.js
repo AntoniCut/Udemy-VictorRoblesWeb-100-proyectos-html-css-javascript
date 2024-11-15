233505baca70e4f0d90490e49e13efae1e9eba66
Antonio <antonicut@gmail.com>
2024-11-15 19:45:16 +0100

    Add - proyecto 9 JS

--- 06-proyectos-javascript/proyecto-09-js/assets/js/09-main.js
+++ 06-proyectos-javascript/proyecto-09-js/assets/js/09-main.js
@@ -1,47 +1,57 @@
 //  **********  /04-curso-100-proyectos-html-css-js  *********** 
 //  **********  /06-proyectos-javascript/  ********************* 
-//  **********  /proyecto-07-js/assets/js/04-main.js  ********** 
+//  **********  /proyecto-09-js/assets/js/09-main.js  ********** 
 //  ************************************************************
 
 
 /*
-     ----------  Enunciado Proyecto 8  ----------  
+     ----------  Enunciado Proyecto 9  ----------  
 
-    -  Crea una función que devuelva si un número (enviado por el usuario) es par o impar.
-    -  Muestra el Resultado por consola, la pantalla y una ventana.
+    -  Crea una función que sume una cantidad indefinida de números
+       pasados por parámetro.
+    
+    -  Muestra el Resultado en un encabezado de segundo nivel (h2).
+    -  Muestra los números que se han utilizado para el resultado debajo en un párrafo.
 */
 
 
-//  -----  referencias al HTML  -----
-const resultado = document.getElementById('resultado');
+
+//  -----  Referencias al HTML  -----
+const resultBox = document.getElementById('result');
 const h2 = document.createElement('h2');
 
+const numbersBox = document.getElementById('numbers');
+const p = document.createElement('p');
 
-const esPar = (numero) => {
-    return numero % 2 === 0;
-};
-
-
-const renderResultado = (numero, parImpar) => {
-
-    const result = `El número ${numero} es ${parImpar}`;
-        console.log(result);
-        alert(result);
-
-        h2.innerHTML = result;
-        resultado.appendChild(h2);
-}
+//  -----  Inicialización de la variable del resultado  -----
+let result = 0;
 
 
-//  -----  introducir número  -----
-let numero = parseInt(prompt('Introduce un Número', 0));
+//  -----  Función que genera números aleatorios  -----
+const generateRandomNumbers = (count, min, max) => Array.from({ length: count }, () => Math.floor(Math.random() * (max - min + 1)) + min);
 
-//  -----  Verificar si el valor introducido no es un número  -----
-if (isNaN(numero)) alert("No has introducido un número válido.");
 
-else {
-
-    if (esPar(numero)) renderResultado(numero, 'par')
-    else renderResultado(numero, 'impar')
+//  -----  Función que suma todos los números  -----
+const sumAll = (...numbers) => {
     
+    p.innerHTML = '<br>Los números son...';
+    
+    numbers.forEach((number, index) => {
+        
+        if (index === numbers.length - 1) p.innerText += `${number}`;
+        else p.innerText += `${number}, `;
+        result += number;
+    });
+
+    h2.innerHTML = `El resultado es: ${result} <br><br>`;
+    resultBox.appendChild(h2);
+    numbersBox.appendChild(p);
 }
+
+
+//  -----  Generar números aleatorios  -----
+const randomNumbers = generateRandomNumbers(5, 1, 100);     // 5 números aleatorios entre 1 y 100.
+
+
+//  -----  Llamar a la función sumAll con los números generados  -----
+sumAll(...randomNumbers);
